4dc91e2adce47113e341512db415f8ef1b648dc7
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-20 00:51:36 +0100

    routes

--- routes/companies.js
+++ routes/companies.js
@@ -1,5 +1,18 @@
 var router = require('express').Router();
 
-router.get('/', require('../controllers/companies/'));
+//Devuelve todas las empresas
+router.get('/', require('../controllers/company/getAllCompany'));
+
+//Devuelve una empresa en concreto
+router.get('/:id', require('../controllers/company/getCompanys'));
+
+//Crea una empresa
+router.post('/create', require('../controllers/company/createCompany'));
+
+//Modifica una empresa
+router.patch('/mod/:id' , require('../controllers/company/modCompany'));
+
+//Borrar empresa
+router.delete('/:id', require('../controllers/company/deleteCompany'));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,18 @@
 var router = require('express').Router();
 
-router.get('/', require('../controllers/users/'));
+//Devuelve todos los usuarios
+router.get('/', require('../controllers/users/getAllUsers'));
+
+//Devuelve un usuario en concreto
+router.get('/:id', require('../controllers/users/getUser'));
+
+//Crea un usuario
+router.post('/create', require('../controllers/users/createUser'));
+
+//Modifica un usuario
+router.patch('/mod/:id' , require('../controllers/users/modUser'));
+
+//Borrar usuario
+router.delete('/:id', require('../controllers/users/deleteUser'));
 
 module.exports = router;
