28918a68971d207722448f35bf1c92d0e877a4e6
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-19 13:00:50 +0100

    añadidos default en models, igualado nombre de clase con nombre de archivo y modificado gitignore

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
-class Usuario{
+class User{
     constructor(usuario, userpass, email){
         this.usuario = usuario
         this.userpass = userpass
         this.email = email
     }
 }
+
+export default User
--- models/Worker.js
+++ models/Worker.js
@@ -1,7 +1,10 @@
-class Empleado{
+class Worker{
     constructor(usuario, userpass){
         this.usuario = usuario
         this.userpass = userpass
         this.email = email
     }
 }
+
+
+export default Worker
