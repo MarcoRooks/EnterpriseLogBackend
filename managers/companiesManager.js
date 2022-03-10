561fbe56e9064e9d34df4b1dbf8aa9783b07f3f4
Miguel Cid <miguel.cid.paz@gmail.com>
2022-03-10 20:24:05 +0100

    añadidos try catch en managers

--- managers/companiesManager.js
+++ managers/companiesManager.js
@@ -5,54 +5,76 @@ const Company = require("../models/Company");
 
 
 
-class companiesManager{
-    
-    getAll(){
-        const companies = client.query("SELECT * FROM companies;");
-        newCompanies = companies.map((company) => new Company(company));
-        client.end();
-        return newCompanies;
-      }
+class companiesManager {
 
-    getByID(){
-        const companies = client.query(`SELECT * FROM companies WHERE idcompany = ${idcompany};`);
-
-        client.end()
-        return newCompanies;
+  getAll() {
+    try {
+      const companies = client.query("SELECT * FROM companies;");
+      newCompanies = companies.map((company) => new Company(company));
+      return newCompanies;
+    } catch (e) {
+      return false
+    } finally {
+      client.end();
     }
+  }
 
-    createNew(){
-      const {name_description, sector, creation_date, logo, webpage, phone_number, social_media, company_value, num_employees, images} = req.body
-
-      const newComp = client.query(`INSERT INTO companies (name_description, sector, creation_date, logo, webpage, phone_number, social_media, company_value, num_employees, images) VALUE (${name_description}, ${sector}, ${creation_date}, ${logo}, ${webpage}, ${phone_number}, ${social_media}, ${company_value}, ${num_employees}, ${images});`);
-
+  getByID() {
+    try {
+      const companies = client.query(`SELECT * FROM companies WHERE idcompany = ${idcompany};`);
+      return newCompanies;
+    } catch (e) {
+      return false
+    } finally {
       client.end()
+    }
+  }
+
+  createNew() {
+    try {
+      const { name_description, sector, creation_date, logo, webpage, phone_number, social_media, company_value, num_employees, images } = req.body
+      const newComp = client.query(`INSERT INTO companies (name_description, sector, 
+        creation_date, logo, webpage, phone_number, social_media, 
+        company_value, num_employees, images) 
+        VALUE (${name_description}, ${sector}, ${creation_date}, ${logo}, ${webpage}, 
+          ${phone_number}, ${social_media}, ${company_value}, ${num_employees}, ${images});`);
       return newComp
-    }
-
-    delete(){
-      const deleteComp = client.query(`DELETE FROM companies WHERE idcompany = ${idcompany} ;`)
-
+    } catch (e) {
+      return false
+    } finally {
       client.end()
+    }
+  }
+
+  delete() {
+    try {
+      const deleteComp = client.query(`DELETE FROM companies WHERE idcompany = ${idcompany} ;`)
       return deleteComp
-
+    } catch (e) {
+      return false
+    } finally {
+      client.end()
     }
+  }
 
-    mod(){
+  mod() {
+    try {
       const modComp = client.query(`UPDATE companies SET 
-        name_description = ${name_description}, 
-        sector = ${sector}, 
-        creation_date = ${creation_date}, 
-        logo = ${logo}, 
-        webpage = ${webpage}, 
-        phone_number = ${phone_number}, 
-        social_media = ${social_media}, 
-        company_value = ${company_value}, 
-        num_employees = ${num_employees}, 
-        images = ${images}`)
-        
-        client.end()
-        return modComp;
+      name_description = ${name_description}, 
+      sector = ${sector}, 
+      creation_date = ${creation_date}, 
+      logo = ${logo}, 
+      webpage = ${webpage}, 
+      phone_number = ${phone_number}, 
+      social_media = ${social_media}, 
+      company_value = ${company_value}, 
+      num_employees = ${num_employees}, 
+      images = ${images}`)
+      return modComp;
+    } catch (e) {
+      return false
+    } finally {
+      client.end()
     }
-
+  }
 }
