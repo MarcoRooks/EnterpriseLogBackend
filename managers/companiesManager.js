6608e4e19753b2eda2a0e9918cdda1d8e9064d9a
Miguel Cid <miguel.cid.paz@gmail.com>
2022-03-12 02:29:53 +0100

    crear company arreglada, creo

--- managers/companiesManager.js
+++ managers/companiesManager.js
@@ -57,7 +57,6 @@ class companiesManager {
       '${data.num_employees}', 
       '${data.images}');`);
 
-      console.log(newComp)
       return newComp
     } catch (e) {
       console.log("falla", e)
